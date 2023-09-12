4840e84415ba18b82308d510096514153a385f6e
MOAK <whdydgus9870@gmail.com>
2023-09-12 17:37:59 +0900

    2023/09/12

--- src/src/ppt1/MainClass.java
+++ src/src/ppt1/MainClass.java
@@ -1,19 +1,38 @@
 package src.ppt1;
 
+import java.util.Scanner;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		int num1 = 1;
-		float num2 = 1.1f;
-		double num3 = 1.2;
+		boolean younyear = isYounYear();
 		
-		System.out.println(num1);
-		System.out.println(num2);
-		System.out.println(num3);
-		
-		if((int)10 == (short)10) {
-			System.out.println("10은 short형이다.");
+		if(younyear == true) {
+			System.out.println("윤년입니다.");
+		}
+		else {
+			System.out.println("윤년이 아닙니다.");
+		}
+	}
+
+
+	public static boolean isYounYear() {
+	
+		try(Scanner scan = new Scanner(System.in)){
+			System.out.println("년도를 입력하세요");
+			int c = scan.nextInt();
+			
+			boolean year;
+			
+			if(c % 4 == 0) {
+				if(c % 100 != 0 || c % 400 == 0) {
+					year = true;
+					return year;
+				}
+			}
+			year = false;
+			return year;
 		}
 	}
 }
