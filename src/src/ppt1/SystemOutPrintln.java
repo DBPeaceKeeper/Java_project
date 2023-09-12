a952d024bb53808609a6f0fe1dddf3edcb8959bc
MOAK <whdydgus9870@gmail.com>
2023-09-12 13:56:08 +0900

    test commit

--- src/src/ppt1/SystemOutPrintln.java
+++ src/src/ppt1/SystemOutPrintln.java
@@ -4,6 +4,6 @@ public class SystemOutPrintln {
  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕!!");
+		System.out.println("퉁퉁이 엄마는 모퉁이!!");
 	}
 }
